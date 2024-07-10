989cf7a858c5d9475aa7d0357e92a957c2e711bf
Mohaz <107796482+Mohaz24@users.noreply.github.com>
2024-07-10 21:12:54 +1000

    .

--- components/Main.jsx
+++ components/Main.jsx
@@ -15,7 +15,7 @@ const Main = () => {
           <h1 className='py-4 text-gray-700'>
             Hi, I&#39;m <span className='text-[#5651e5]'> Mohammed.M</span>
           </h1>
-          <h1 className='py-2 text-gray-700'>A Front-End Web Developer</h1>
+          <h1 className='py-2 text-gray-700'>A Full-Stack Web Developer</h1>
           <p className='py-4 text-gray-600 sm:max-w-[70%] m-auto'>
             I’m focused on building responsive front-end web applications
             integrating back-end technologies.
